bdd9fb23f304b848733935e2dd7496c6baf88dc6
nilubhai93 <niladrimaji9382@gmail.com>
2025-02-25 18:37:20 +0530

    weekly assignment

---
+++ Day 11/ClassWork/script.js
@@ -0,0 +1,28 @@
+// let obj={
+//     name:"Nilu Maji"
+// }
+
+
+
+// num = 30;
+// if (num%3==0 && num%5==0){
+//     console.log("Fizz Buzz")
+// }
+// else if (num%3==0){
+//     console.log("fizz")
+// }
+// else if (num%5==0){
+//     console.log("fizz")
+// }
+
+
+
+arr=[1,2,3,4,5,6,7,8,9,10];
+// for loop(even/odd)
+for(let num =0; num<arr.length; num++){
+    if (arr[num]%2==0){
+        console.log(`number is even ${num}`)
+    }else{
+        console.log(`number is odd ${num}`)
+    }
+}
